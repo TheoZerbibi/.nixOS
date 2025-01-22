99e938c91aa5a68b15d456069c1f7f7e1f14c602
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-22 12:01:12 +0100

    Fix rebuild

--- users/tzeribi/fontconfig.nix
+++ users/tzeribi/fontconfig.nix
@@ -25,25 +25,20 @@
       ];
     };
 
-    extraConfig = ''
+    xdg.configFile."fontconfig/fonts.conf".text = ''
       <?xml version='1.0'?>
       <!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
       <fontconfig>
-        <!-- Active l'antialiasing -->
         <match target="font">
           <edit name="antialias" mode="assign">
             <bool>true</bool>
           </edit>
         </match>
-
-        <!-- Active le hinting léger -->
         <match target="font">
           <edit name="hintstyle" mode="assign">
             <const>hintslight</const>
           </edit>
         </match>
-
-        <!-- Définit le rendu en subpixel (LCD) -->
         <match target="font">
           <edit name="rgba" mode="assign">
             <const>rgb</const>
