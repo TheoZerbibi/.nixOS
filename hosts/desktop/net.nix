d48a257ac6a9b8017ef648b4ee3263f432d5256a
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-22 11:06:53 +0100

    Fix rebuild

--- hosts/desktop/net.nix
+++ hosts/desktop/net.nix
@@ -1,10 +1,4 @@
 {hostname, ...}: {
-
-  environment.systemPackages = with pkgs; [
-    networkmanager
-  ];
-  networking.networkmanager.enable = true;
-
   networking = {
     hostName = hostname;
     networkmanager.enable = true;
