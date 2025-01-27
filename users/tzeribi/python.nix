e678051b6899c180463e7677b51cd60bb7c385db
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 16:43:58 +0100

    add pywolf

--- users/tzeribi/python.nix
+++ users/tzeribi/python.nix
@@ -7,7 +7,7 @@
         version = "0.3.5";
         src = pkgs.fetchurl {
           url = "https://files.pythonhosted.org/packages/b7/cc/c31688b9d5d4ddc73d713cde14c5ab09a95046fb0ede2e6fa7ee7e904900/pywolf-0.3.5.tar.gz";
-          sha256 = "Nx0NdRTQtypL7KGLA5iK42mSVtCfVboCQJ92YahpaOY";
+          sha256 = "0cv6v11ygrljcfy6w2pzi8418wk4yrx10xrpbahkciwcij3p71bj";
         };
         doCheck = false;
         propagatedBuildInputs = [];
