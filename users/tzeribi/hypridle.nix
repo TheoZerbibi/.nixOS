dfdea9c3e208c2a0057b0b547daa79e545501b2c
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 12:46:27 +0100

    Fix rebuild,

--- users/tzeribi/hypridle.nix
+++ users/tzeribi/hypridle.nix
@@ -1,13 +1,39 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
-  systemd.user.services.hypridle = {
+  services.hypridle = {
+    enable = true;
 
-    serviceConfig = {
-      ExecStart = "${pkgs.hypridle}/bin/hypridle --config $HOME/.cache/wal/colors-hyprland";
-      Restart = "always";
-      RestartSec = "5s";
+    settings = {
+      source = "$HOME/.cache/wal/colors-hyprland";
+
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        ignore_dbus_inhibit = "/opt/spotify/spotify";
+      };
+
+      listeners = [
+        {
+          timeout = 270;
+          on_timeout = ''
+            source "$HOME/.cache/wal/colors-hyprland"
+            notify-send "System" "You are about to be locked out!" -i "$wallpaper"
+          '';
+        }
+        {
+          timeout = 600;
+          on_timeout = "loginctl lock-session";
+          on_resume = ''
+            sleep 2
+            source "$HOME/.cache/wal/colors-hyprland"
+            notify-send "System" "Unlocked! Hey $USER" -i "$wallpaper"
+          '';
+        }
+        {
+          timeout = 1200;
+          on_timeout = "hyprctl dispatch dpms off";
+          on_resume = "hyprctl dispatch dpms on";
+        }
+      ];
     };
-
-    wantedBy = [ "default.target" ];
   };
 }
