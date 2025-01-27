753fcae1f9521bbb0ade4766747db397fc8c7b04
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 12:39:48 +0100

    Fix rebuild,

--- users/tzeribi/hypridle.nix
+++ users/tzeribi/hypridle.nix
@@ -2,7 +2,6 @@
 {
   systemd.user.services.hypridle = {
     enable = true;
-    description = "Hypridle idle management service";
 
     serviceConfig = {
       ExecStart = "${pkgs.hypridle}/bin/hypridle --config $HOME/.cache/wal/colors-hyprland";
