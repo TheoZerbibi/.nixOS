336fe73f70218dad51bf5d2f3735319a3b0f8313
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 16:35:36 +0100

    add pywolf

--- users/tzeribi/python.nix
+++ users/tzeribi/python.nix
@@ -4,7 +4,7 @@
       pip
       (ps.buildPythonPackage rec {
         pname = "pywalfox";
-        version = "2.0.11";
+        version = "0.3.5";
         src = fetchPypi {
           inherit pname version;
           sha256 = "0cv6v11ygrljcfy6w2pzi8418wk4yrx10xrpbahkciwcij3p71bj";
