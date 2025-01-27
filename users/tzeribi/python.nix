63a1f7af6c17a7fc7746714189516b196c8038d6
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 16:30:15 +0100

    add pywolf

--- users/tzeribi/python.nix
+++ users/tzeribi/python.nix
@@ -1,18 +1,16 @@
-{nixpkgs, pkgs, lib, ...}: {
-  home.packages = with nixpkgs.unstable; [
-    (pkgs.python3.withPackages (ps: with ps; [
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    (python3.withPackages (ps: with ps; [
       pip
+      (ps.buildPythonPackage rec {
+        pname = "pywalfox";
+        version = "2.0.11";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "0gr3rczbd5hj3q6gk10biig8nv3mzxb8k4ywpfzps9y6hrfl9xl6";
+        };
+        propagatedBuildInputs = [];
+      })
     ]))
   ];
-  home.activation.installPywalfox = {
-    text = ''
-      #!/usr/bin/env bash
-      if ! pip show pywalfox &>/dev/null; then
-        echo "Installing pywalfox..."
-        pip install --user pywalfox
-      fi
-    '';
-    enable = true;
-  };
-
 }
