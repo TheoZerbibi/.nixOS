8964fbfcb150f7579b838413e237617220b3b25f
Theo ZERIBI <theo.zeribi@invisensing.io>
2025-01-27 17:09:35 +0100

    add pywolf

--- users/tzeribi/python.nix
+++ users/tzeribi/python.nix
@@ -1,17 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    pywalfox-native
     (python3.withPackages (ps: with ps; [
       pip
-      (ps.buildPythonPackage rec {
-        pname = "pywalfox";
-        version = "0.3.5";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Frewacom/pywalfox/releases/download/v2.0.11/pywalfox-2.0.11.zip";
-          sha256 = "0839e6c003341e192a7be1802fa40d8b05842dad1a3a3beac88f67fba8149dec";
-        };
-        doCheck = false;
-        nativeBuildInputs = [ ps.flit ];
-      })
     ]))
   ];
 }
